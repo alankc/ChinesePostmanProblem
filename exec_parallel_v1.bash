cf77faaab3835cd4793d9931bcbff4c40c9bcb88
Alan Kunz Cechinel <cechinel.a.k@gmail.com>
2021-08-30 19:28:40 -0300

    number of reps

--- exec_parallel_v1.bash
+++ exec_parallel_v1.bash
@@ -1,3 +1,4 @@
+rm -f cpp_problem
 g++ common/src/* parallel-v1/src/* -o cpp_problem -fopenmp -O2
 
 export OMP_NUM_THREADS=6
@@ -7,38 +8,38 @@ taskset --cpu-list 0-5 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 5 2 0 >>
 export OMP_NUM_THREADS=6
 printf "\n\n OMP_NUM_THREADS=6 \n\n"
 printf "\nv2_5reps_0n_1p_6odd"
-taskset --cpu-list 0-5 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 0 1 >> results/Par/v1/6th/v2_10reps_0n_1p_6odd.txt
+taskset --cpu-list 0-5 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 0 1 >> results/Par/v1/6th/v2_10reps_0n_1p_6odd.txt
 printf "\nv2_5reps_1n_0p_10odd"
-taskset --cpu-list 0-5 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 1 0 >> results/Par/v1/6th/v2_10reps_1n_0p_10odd.txt
+taskset --cpu-list 0-5 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 1 0 >> results/Par/v1/6th/v2_10reps_1n_0p_10odd.txt
 printf "\nv2_5reps_2n_0p_16odd"
-taskset --cpu-list 0-5 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 2 0 >> results/Par/v1/6th/v2_10reps_2n_0p_16odd.txt
+taskset --cpu-list 0-5 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 2 0 >> results/Par/v1/6th/v2_10reps_2n_0p_16odd.txt
 printf "\nv2_5reps_2n_1p_18odd"
-taskset --cpu-list 0-5 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 2 1 >> results/Par/v1/6th/v2_10reps_2n_1p_18odd.txt
+taskset --cpu-list 0-5 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 2 1 >> results/Par/v1/6th/v2_10reps_2n_1p_18odd.txt
 printf "\nv2_5reps_2n_2p_20odd"
-taskset --cpu-list 0-5 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 2 2 >> results/Par/v1/6th/v2_10reps_2n_2p_20odd.txt
+taskset --cpu-list 0-5 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 2 2 >> results/Par/v1/6th/v2_10reps_2n_2p_20odd.txt
 
 export OMP_NUM_THREADS=4
 printf "\n\n OMP_NUM_THREADS=4 \n\n"
 printf "\nv2_5reps_0n_1p_6odd"
-taskset --cpu-list 0-3 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 0 1 >> results/Par/v1/4th/v2_10reps_0n_1p_6odd.txt
+taskset --cpu-list 0-3 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 0 1 >> results/Par/v1/4th/v2_10reps_0n_1p_6odd.txt
 printf "\nv2_5reps_1n_0p_10odd"
-taskset --cpu-list 0-3 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 1 0 >> results/Par/v1/4th/v2_10reps_1n_0p_10odd.txt
+taskset --cpu-list 0-3 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 1 0 >> results/Par/v1/4th/v2_10reps_1n_0p_10odd.txt
 printf "\nv2_5reps_2n_0p_16odd"
-taskset --cpu-list 0-3 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 2 0 >> results/Par/v1/4th/v2_10reps_2n_0p_16odd.txt
+taskset --cpu-list 0-3 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 2 0 >> results/Par/v1/4th/v2_10reps_2n_0p_16odd.txt
 printf "\nv2_5reps_2n_1p_18odd"
-taskset --cpu-list 0-3 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 2 1 >> results/Par/v1/4th/v2_10reps_2n_1p_18odd.txt
+taskset --cpu-list 0-3 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 2 1 >> results/Par/v1/4th/v2_10reps_2n_1p_18odd.txt
 printf "\nv2_5reps_2n_2p_20odd"
-taskset --cpu-list 0-3 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 2 2 >> results/Par/v1/4th/v2_10reps_2n_2p_20odd.txt
+taskset --cpu-list 0-3 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 2 2 >> results/Par/v1/4th/v2_10reps_2n_2p_20odd.txt
 
 export OMP_NUM_THREADS=2
 printf "\n\n OMP_NUM_THREADS=2 \n\n"
 printf "\nv2_5reps_0n_1p_6odd"
-taskset --cpu-list 0-1 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 0 1 >> results/Par/v1/2th/v2_10reps_0n_1p_6odd.txt
+taskset --cpu-list 0-1 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 0 1 >> results/Par/v1/2th/v2_10reps_0n_1p_6odd.txt
 printf "\nv2_5reps_1n_0p_10odd"
-taskset --cpu-list 0-1 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 1 0 >> results/Par/v1/2th/v2_10reps_1n_0p_10odd.txt
+taskset --cpu-list 0-1 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 1 0 >> results/Par/v1/2th/v2_10reps_1n_0p_10odd.txt
 printf "\nv2_5reps_2n_0p_16odd"
-taskset --cpu-list 0-1 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 2 0 >> results/Par/v1/2th/v2_10reps_2n_0p_16odd.txt
+taskset --cpu-list 0-1 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 2 0 >> results/Par/v1/2th/v2_10reps_2n_0p_16odd.txt
 printf "\nv2_5reps_2n_1p_18odd"
-taskset --cpu-list 0-1 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 2 1 >> results/Par/v1/2th/v2_10reps_2n_1p_18odd.txt
+taskset --cpu-list 0-1 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 2 1 >> results/Par/v1/2th/v2_10reps_2n_1p_18odd.txt
 printf "\nv2_5reps_2n_2p_20odd"
-taskset --cpu-list 0-1 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 10 2 2 >> results/Par/v1/2th/v2_10reps_2n_2p_20odd.txt
+taskset --cpu-list 0-1 sudo -S <<< "281094" nice -n -19 ./cpp_problem 2 20 2 2 >> results/Par/v1/2th/v2_10reps_2n_2p_20odd.txt
