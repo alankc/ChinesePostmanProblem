335d2f8a0a47bef7fff2834991a6b22f58df11f5
Alan Kunz Cechinel <cechinel.a.k@gmail.com>
2021-09-02 18:49:44 -0300

    change compilation

--- exp-min-seq.bash
+++ exp-min-seq.bash
@@ -1,9 +1,9 @@
 root="results-g5/par/v2_min_seq"
 header="n_odds\tisEulerian\tlistPairs\ttime_tmp\ttime_for\tlistPairsCombinationsBase\tmodifyGraph\thierholzer\ttotal_time\ttotal_distance\tpath\n"
 
-printf "\nCompilling-cpp_sequential\n"
-rm -f cpp_sequential
-g++ common/src/* sequential/src/* -o cpp_sequential -O2
+printf "\nCompilling-cpp_parallel_v2\n"
+rm -f cpp_parallel_v2
+g++ common/src/* parallel-v2/src/* -o cpp_parallel_v2 -O2
 
 for min_seq in 6 8 10 12
 do
@@ -18,10 +18,10 @@ do
     for i in {1..50}
     do
         printf "$min_seq cpp_sequential-%d\n" $i
-        taskset --cpu-list 0 sudo -S <<< "281094" nice -n -19 ./cpp_sequential 0 1 >> $path/6odd.txt
-        taskset --cpu-list 0 sudo -S <<< "281094" nice -n -19 ./cpp_sequential 1 0 >> $path/10odd.txt
-        taskset --cpu-list 0 sudo -S <<< "281094" nice -n -19 ./cpp_sequential 2 0 >> $path/16odd.txt
-        taskset --cpu-list 0 sudo -S <<< "281094" nice -n -19 ./cpp_sequential 2 1 >> $path/18odd.txt
-        taskset --cpu-list 0 sudo -S <<< "281094" nice -n -19 ./cpp_sequential 2 2 >> $path/20odd.txt
+        taskset --cpu-list 0 sudo -S <<< "281094" nice -n -19 ./cpp_parallel_v2 0 1 $min_seq >> $path/6odd.txt
+        taskset --cpu-list 0 sudo -S <<< "281094" nice -n -19 ./cpp_parallel_v2 1 0 $min_seq >> $path/10odd.txt
+        taskset --cpu-list 0 sudo -S <<< "281094" nice -n -19 ./cpp_parallel_v2 2 0 $min_seq >> $path/16odd.txt
+        taskset --cpu-list 0 sudo -S <<< "281094" nice -n -19 ./cpp_parallel_v2 2 1 $min_seq >> $path/18odd.txt
+        taskset --cpu-list 0 sudo -S <<< "281094" nice -n -19 ./cpp_parallel_v2 2 2 $min_seq >> $path/20odd.txt
     done
 done
