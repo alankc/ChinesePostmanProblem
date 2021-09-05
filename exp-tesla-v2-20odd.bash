2028e961befe04b9080bbc16b54f90fcb02f8e67
Alan Kunz Cechinel <cechinel.a.k@gmail.com>
2021-09-05 17:48:44 -0300

    change to continue from 16

--- exp-tesla-v2-20odd.bash
+++ exp-tesla-v2-20odd.bash
@@ -13,7 +13,17 @@ printf "\nCompilling-cpp_parallel_v2-exp-tesla-v2\n"
 rm -f cpp_parallel_v2
 g++ common/src/* parallel-v2/src/* -o cpp_parallel_v2 -fopenmp -O2 -DGUIDED
 
-for ((t=2; t<=$threads; t=t+2))
+t=16
+let core=$t-1
+export OMP_NUM_THREADS=$t
+printf "$header" >> $path/"$t"_th.txt
+for ((i=1; i<=8; i++))
+do
+	printf "$t threads -- rep $i\n"
+	taskset --cpu-list 0-$core ./cpp_parallel_v2 2 2 $min_seq >> $path/"$t"_th.txt
+done
+
+for ((t=17; t<=$threads; t=t+2))
 do
 	let core=$t-1
 	export OMP_NUM_THREADS=$t
