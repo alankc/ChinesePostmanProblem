09fcbff8cacf5dd9d29143cad4a1976b1ced91f0
Alan Kunz Cechinel <cechinel.a.k@gmail.com>
2021-09-22 20:09:29 -0300

    add comments

--- sequential/include/ChinesePostmanProblem.hpp
+++ sequential/include/ChinesePostmanProblem.hpp
@@ -9,19 +9,51 @@ class ChinesePostmanProblem
 {
 private:
     Multigraph *mg;
+
+    /**
+     * Receives a list of odd vertices and generates a list of distances, and a list of paths. Basically a complete graph.
+     * For example, with the vertices 1, 2, 3, 4. It generates distances and paths to pairs: (1,2), (1,3), (1,4), (2,3), (2,4), (3,4). 
+    **/ 
     void listPairs(vector<uint16_t> &oddVertices, vector<vector<uint16_t>> &distances, vector<map<uint16_t, list<uint16_t>>> &paths);
+
+    /**
+     * Generate pairs combinations.
+     * For example, with the vertices 1, 2, 3, 4.  It returns {(1,2)(3,4)}, {(1,3)(2,4)} and {(1,4)(2,3)}
+    **/ 
     vector<vector<pair<uint16_t, uint16_t>>> listPairsCombinations(vector<uint16_t> &oddVertices);
+    
+    //Deprecated function. It was used in the initial work. But due to memory, it was not used in the final work.
     vector<pair<uint16_t, uint16_t>> bestPairsCombination(vector<vector<pair<uint16_t, uint16_t>>> &pairCombinations, vector<vector<uint16_t>> &distances);
+    
+    /**
+     * For each pair of vertices in bestPairs, it duplicates edges that are in the path of these vertices.
+    **/  
     void modifyGraph(vector<pair<uint16_t, uint16_t>> &bestPairs, vector<map<uint16_t, list<uint16_t>>> &paths);
 
+    /**
+     * Calls listPairsCombinations aiming to define the best set of pairs combinations
+    **/
     vector<pair<uint16_t, uint16_t>> listPairsCombinationsBase(vector<uint16_t> &oddVertices, vector<vector<uint16_t>> &distances);
+    
+    /**
+     * Given a list of pairs combinations, it sum the distance in the path of each pair
+     * Example: 
+     *  pairCombinations: {(1,2)(3,4)}
+     *  distances: (1,2) = 3, (3,4) = 5 
+     *  it would return 8
+    **/
     uint16_t distancePairCombination(vector<pair<uint16_t, uint16_t>> &pairCombinations, vector<vector<uint16_t>> &distances);
     
 
 public:
     ChinesePostmanProblem(/* args */);
     ~ChinesePostmanProblem();
+    //Deprecated function. 
     void solve(Multigraph *mg, uint16_t startVertex);
+    /**
+     * Solver
+     * It prints time spent in each function, the total time, and the eulerian cycle.
+    **/ 
     void solve_v2(Multigraph *mg, uint16_t startVertex);
 };
 
